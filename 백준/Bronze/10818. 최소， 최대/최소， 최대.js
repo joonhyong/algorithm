194fc5ed35e8b85e5b59e19a780f6bdc2abf12c4
JunHyeong Park <64461926+joonhyong@users.noreply.github.com>
2025-09-23 17:41:51 +0900

    [Bronze III] Title: 최소, 최대, Time: 372 ms, Memory: 96752 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,10 +1,21 @@
+// 백준 2562번 최댓값
+/*입력예제
+5
+20 10 35 30 7
+*/
+/*유도코드
+- Math 내장객체 사용
+입력예제를 저장하는 배열 생성
+배열에서 최댓값, 최솟값 추출
+*/
+
 const fs = require("fs");
-const file = process.platform === "linux" ? "/dev/stdin" : "./exam.txt";
+const file = process.platform === "linux" ? "/dev/stdin" : "../exam.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
 
-const N = input[0];
-let arr = [];
-arr = input[1].split(" ").map(Number);
-const min = Math.min(...arr);
-const max = Math.max(...arr);
-console.log(min, max);
+const n = Number(input.shift());
+const numbers = input.shift().split(" ").map(Number);
+
+const maxNum = Math.max(...numbers);
+const minNum = Math.min(...numbers);
+console.log(minNum+" "+maxNum);
