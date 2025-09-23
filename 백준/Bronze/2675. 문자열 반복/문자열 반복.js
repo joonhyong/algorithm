026d2e1f59111ddfd5cbdde06402c4289f7c9400
JunHyeong Park <64461926+joonhyong@users.noreply.github.com>
2025-09-23 19:47:04 +0900

    [Bronze II] Title: 문자열 반복, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -5,9 +5,13 @@
 5 /HTP
 */
 /*유도코드
+(1) 문자열 인덱싱 + for문
 문자열을 배열로 변환
 각각의 인덱스별로 for문으로 반복된 새로운 문자열을 생성
 각각의 인덱스를 합쳐서 출력
+(2) repeat() 메서드
+반복대상 문자열을 배열로 변환
+배열의 각 요소에 repeat() 호출하여 
 */
 
 const fs = require("fs");
@@ -16,13 +20,22 @@ const input = fs.readFileSync(file).toString().trim().split("\n");
 
 const T = Number(input.shift());
 
+// for (let i = 0; i < T; i++) {
+//   const [n, str] = input.shift().split(" ");
+//   let answer = "";
+//   for (let j = 0; j < str.length; j++) {
+//     for (let k = 0; k < n; k++) {
+//       answer += str[j];
+//     }
+//   }
+//   console.log(answer);
+// }
+
 for (let i = 0; i < T; i++) {
   const [n, str] = input.shift().split(" ");
   let answer = "";
   for (let j = 0; j < str.length; j++) {
-    for (let k = 0; k < n; k++) {
-      answer += str[j];
-    }
+    answer += str[j].repeat(n);
   }
   console.log(answer);
 }
