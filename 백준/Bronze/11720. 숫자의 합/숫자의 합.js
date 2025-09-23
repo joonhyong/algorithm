edf05264c426c555c663b6b94afc7574bf4fd994
JunHyeong Park <64461926+joonhyong@users.noreply.github.com>
2025-09-23 16:22:18 +0900

    [Bronze IV] Title: 숫자의 합, Time: 100 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.js
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -1,11 +1,27 @@
+// 백준 11720번 숫자의 합
+/*입력예제
+5
+54321
+*/
+
 const fs = require("fs");
-const file = process.platform === "linux" ? "/dev/stdin" : "./exam.txt";
+const file = process.platform === "linux" ? "/dev/stdin" : "../exam.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
 
-const N = Number(input[0]);
-let numbers = input[1].trim().toString().split("").map(Number);
+const n = Number(input.shift());
+
+// 두번째 열의 숫자->문자열하고, 한자리씩 떼어오기
+// let sum = 0;
+// let numbers = input.shift();
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += Number(numbers[i]);
+// }
+// console.log(sum);
+
+// 두번째의 열을 배열로 변환하고 forEach문 사용
+let numbers = input.shift().split("").map(Number);
 let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-  sum += numbers[i];
-}
+numbers.forEach((num) => {
+  sum += num;
+});
 console.log(sum);
